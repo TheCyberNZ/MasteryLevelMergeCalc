09f7fc1d173ba4d7251319be623700d6e5fc73ee
TheCyberNZ <matthias.campbell2005@gmail.com>
2025-01-19 21:02:46 +1300

    made text boxes work
    will add calculation function tomorrow

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let totalPosition;
+let totalPositions;
 
 document.getElementById("totalPositionSubmit").onclick = function(){
-totalPositions = document.getElementById("totalPosition").ariaValueMax;
-console.log(totalPositions)
+totalPositions = document.getElementById("totalPosition").value;
+document.getElementById("myTotalPositions").textContent = `Current Total Positions:  ${totalPositions}`
+}
+let targetMasteryLevel;
 
+document.getElementById("masteryLevelSubmit").onclick = function(){
+targetMasteryLevel = document.getElementById("masteryLevel").value;
+document.getElementById("myMasteryLevel").textContent = `Target Mastery level:  ${targetMasteryLevel}`
 }
