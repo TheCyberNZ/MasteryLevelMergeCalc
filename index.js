377e26679b3a5e5add0881ebc7e0d74fffe0085b
TheCyberNZ <matthias.campbell2005@gmail.com>
2025-01-20 15:59:56 +1300

    added functionality
    title

--- index.js
+++ index.js
@@ -1,12 +1,99 @@
 let totalPositions;
-
-document.getElementById("totalPositionSubmit").onclick = function(){
-totalPositions = document.getElementById("totalPosition").value;
-document.getElementById("myTotalPositions").textContent = `Current Total Positions:  ${totalPositions}`
-}
+let mergesNeeded;
 let targetMasteryLevel;
+let reduction;
 
-document.getElementById("masteryLevelSubmit").onclick = function(){
-targetMasteryLevel = document.getElementById("masteryLevel").value;
-document.getElementById("myMasteryLevel").textContent = `Target Mastery level:  ${targetMasteryLevel}`
+document.getElementById("totalPositionSubmit").onclick = function () {
+    totalPositions = document.getElementById("totalPosition").value;
+    document.getElementById("myTotalPositions").textContent = `Current Total Positions:  ${totalPositions}`;
+}
+
+document.getElementById("masteryLevelSubmit").onclick = function () {
+    targetMasteryLevel = document.getElementById("masteryLevel").value;
+    document.getElementById("myMasteryLevel").textContent = `Target Mastery level:  ${targetMasteryLevel}`;
+}
+
+document.getElementById("calculate").onclick = function () {
+
+    targetMasteryLevel = parseFloat(targetMasteryLevel) || 0;
+
+
+    reduction = 0.9999 ** totalPositions;
+
+
+
+    if (targetMasteryLevel === 0) {
+        mergesNeeded = 0;
+    }
+    else if (targetMasteryLevel === 1) {
+        mergesNeeded = 250;
+    }
+    else if (targetMasteryLevel === 2) {
+        mergesNeeded = 1000;
+    }
+    else if (targetMasteryLevel === 3) {
+        mergesNeeded = 3000;
+    }
+    else if (targetMasteryLevel === 4) {
+        mergesNeeded = 6000;
+    }
+    else if (targetMasteryLevel === 5) {
+        mergesNeeded = 10000;
+    }
+    else if (targetMasteryLevel === 6) {
+        mergesNeeded = 15000;
+    }
+    else if (targetMasteryLevel === 7) {
+        mergesNeeded = 22000;
+    }
+    else if (targetMasteryLevel === 8) {
+        mergesNeeded = 30000;
+    }
+    else if (targetMasteryLevel === 9) {
+        mergesNeeded = 40000;
+    }
+    else if (targetMasteryLevel === 10) {
+        mergesNeeded = 50000;
+    }
+    else if (targetMasteryLevel === 11) {
+        mergesNeeded = 64000;
+    }
+    else if (targetMasteryLevel === 12) {
+        mergesNeeded = 75000;
+    }
+    else if (targetMasteryLevel === 13) {
+        mergesNeeded = 92000;
+    }
+    else if (targetMasteryLevel === 14) {
+        mergesNeeded = 110000;
+    }
+    else if (targetMasteryLevel === 15) {
+        mergesNeeded = 130000;
+    }
+    else if (targetMasteryLevel === 16) {
+        mergesNeeded = 155000;
+    }
+    else if (targetMasteryLevel === 17) {
+        mergesNeeded = 180000;
+    }
+    else if (targetMasteryLevel === 18) {
+        mergesNeeded = 210000;
+    }
+    else if (targetMasteryLevel === 19) {
+        mergesNeeded = 250000;
+    }
+    else if (targetMasteryLevel === 20) {
+        mergesNeeded = 300000;
+    }
+    else {
+        mergesNeeded = 2500 * Math.pow((targetMasteryLevel - 10), 2) + 2500 * targetMasteryLevel;
+    }
+
+
+
+
+
+    document.getElementById("label1").textContent = `You need ${mergesNeeded*reduction}`;
+    document.getElementById("label2").textContent = ` merges to get a barrel to level ${targetMasteryLevel}`;
+    document.getElementById("label3").textContent = ` with a reduction of  ${reduction}`;
 }
